7cb3b9b22f0a658bd0daf15ce5909d6baa232907
thalyson cruz <thalysonbdo@gmail.com>
2023-02-19 19:03:59 -0300

    Defined users routes (routes.rb)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    password: 'password_reset',
+    registration: 'signup'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
