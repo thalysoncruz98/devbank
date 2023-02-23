fcc83b41ab0918050b92253d9709c59a8d5917ae
thalyson cruz <thalysonbdo@gmail.com>
2023-02-23 09:13:01 -0300

    Added accounts migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_19_215412) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_23_120917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
