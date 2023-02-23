d851e44ec5a122dae26dae88a56c90e11eff984f
thalyson cruz <thalysonbdo@gmail.com>
2023-02-23 10:21:58 -0300

    Added cash column in new users migrate

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_23_120917) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "cash", default: 5000.0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
