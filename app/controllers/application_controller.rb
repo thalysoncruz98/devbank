5be7d472dedf7edcded579063f2f83cf347b8362
thalyson cruz <thalysonbdo@gmail.com>
2023-02-23 19:48:53 -0300

    Added redirect after logout (controllers/application_controller)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
           super
         end
     end
+
+    #Redirect after logout
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+    end
 end
