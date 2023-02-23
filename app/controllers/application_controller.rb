adace839f62e0ae5bdc77764dc70766d67947926
thalyson cruz <thalysonbdo@gmail.com>
2023-02-23 12:23:56 -0300

    Added redirect after login (controllers/application_controller)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     protect_from_forgery
+
+    #Redirect after login
+    def after_sign_in_path_for(resource_or_scope)
+        if resource_or_scope.is_a?(User)
+          "/accounts"
+        else
+          super
+        end
+    end
 end
