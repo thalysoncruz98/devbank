02d7830fada0635b379ec3ebdb44a04849cb5d54
thalyson cruz <thalysonbdo@gmail.com>
2023-02-26 16:32:32 -0300

    Added function to lock account (controllers/application_controller)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,11 @@ class ApplicationController < ActionController::Base
     def after_sign_out_path_for(resource_or_scope)
       new_user_session_path
     end
+
+    #Lock account
+    def lock_account
+      current_user.lock_access!
+      sign_out(current_user)
+      redirect_to root_path, notice: "Sua conta foi bloqueada com sucesso."
+    end
 end
